db935b1e09d0526a8dbec2a90b6773e0d2e89cb1
zakiaafrin <zjeme@techlaunch.io>
2020-04-12 18:59:34 -0400

    Updated

--- src/component/App.js
+++ src/component/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';   // style
-import First from './First';
+import Example from './Example';
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: 'Zakia', email: 'abc@xyz.com', address: 'ABC' },
-      { name: 'Afrin', email: 'def@xyz.com', address: 'DEF' },
-      { name: 'Jeme', email: 'ghi@xyz.com', address: 'GHI' }
-    ]
-  }
-
   render() {
     return (
       <div className='App'>
-        {this.state.persons.map((people, index) => {
-          return <First
-            key={index}
-            name={people.name}
-            email={people.email}
-            address={people.address} />
-        })}
+        <Example name='Stateless Component' />
       </div>
     );
   }
---
+++ src/component/Example.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Example = (props) => {
+    return <h1> I am a Stateless component, Name: {props.name}</h1>
+}
+export default Example;
